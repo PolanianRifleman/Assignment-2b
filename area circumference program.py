501c8bf061fa7826b9399b6b3e2e1566f504c117
PolanianRifleman <124378806+PolanianRifleman@users.noreply.github.com>
2023-03-26 19:42:17 -0400

    updated to add a repeat if the user enters and invalid input

--- area circumference program.py
+++ area circumference program.py
@@ -1,18 +1,27 @@
 import math
 
-d = float(input("What is the diameter of your circle? "))
+while True:
 
-r = d/2
+    d = input("What is the diameter of your circle? ")
 
-p = math.pi
+    try:
+        d = float(d)
+    except ValueError:
+        print("INVALID INPUT. ENTER NUMBER")
+        continue
 
-a = float(p * (r ** 2))
+    r = d/2
 
-c = float(2 * 3.1415 * r)
+    p = math.pi
 
-b = str(a)
+    a = float(p * (r ** 2))
 
-d = str(c)
+    c = float(2 * 3.1415 * r)
 
-print("The circumference is " + d)
-print("The area is " + b)
+    b = str(a)
+
+    d = str(c)
+
+    print("The circumference is " + d)
+    print("The area is " + b)
+    break
